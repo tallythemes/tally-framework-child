626add7541489464d2eedac8e238e0cbbe9f3dea
Sazzad Hussain <wedcoder09@gmail.com>
2014-10-13 12:36:30 +0600

    Improve the code standard

--- functions.php
+++ functions.php
@@ -4,15 +4,15 @@
 -------------------------------------------------------*/
 add_filter( 'tally614ba1c62845593400f9fa8ea1517faa', '__return_true' );
 
-add_filter( 'tallykit_docf4e966b90ab1cc1ce61455164c867bc8', '__return_true');
 add_filter( 'tallykit_logoa960064244af68fdda5c8dff6ed56710', '__return_true');
-add_filter( 'tallykit_parallax4d9c5183d7a360c86c4f4f292a5354dc', '__return_true');
 add_filter( 'tallykit_people543023146d784d67c921609b1825ec38', '__return_true');
 add_filter( 'tallykit_portfolio28a5cbad70e7a1cbffe1b4a9d6b6594c', '__return_true');
 add_filter( 'tallykit_shortcodesa6a879a475bf2e0abaf4c4b77cc5a8e5', '__return_true');
 add_filter( 'tallykit_slideshow58cfb02f8e8273e2b1e6bd49d6329ab1', '__return_true');
 add_filter( 'tallykit_testimonialf2456c67473ec153a9ab5e30bf4c5e57', '__return_true');
 add_filter( 'tallykit_galleryae6f1f6116e418c0d988800ca5f95c17', '__return_true');
+add_filter( 'tallykit_buddypress50cf7ce0ada06e45fd7a7ccf46ed5340', '__return_true');
+add_filter( 'tallykit_FrontPagea6ad00a15774817903738705afbefdef', '__return_true');
 
 
 
